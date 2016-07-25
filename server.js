42a6492f63b012a9245b0028e368c93eff9eda1b
shsaci <shsaci@yahoo.com.au>
2016-07-26 08:24:19 +1200

    adding comment, removing extra code

--- server.js
+++ server.js
@@ -9,19 +9,20 @@ var data = require('./views/data')
 var PORT = 3000
 var app = express()
 
+// res.send method
 app.get('/', function (req, res){
   res.send('<h1>Hello you!</h1>')
 })
 
+// using static content
 app.use(express.static('public'))
 
 app.engine('hbs', hbs())
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
+// render page using index.hbs and data file
 app.get('/render', function(req, res) {
-  // console.log(data);
-  // console.log(data.people[0].name);
   res.render('index', data)
 })
 
